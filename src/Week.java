e4d88029c7d419b36d65a22d2e4bb6960006f901
emivulpe <emivulpe@abv.bg>
2013-11-16 00:49:57 +0200

    Week repaired

--- src/Week.java
+++ src/Week.java
@@ -1,19 +1,17 @@
 import java.util.ArrayList;
 
 public class Week {
-
-		private ArrayList<Day> days;
-		
-		public Week(){
-			days = new ArrayList<Day>(5);
-			for (int i=0; i<5; i++){
-				days.add(new Day());
-			}
-		}
-		
-		public Day getDay(int day){
-			return days.get(day);
-		}
-		
+	private ArrayList<Day> days;
 	
+	public Week(){
+		days = new ArrayList<Day>(5);
+		for (int i=0; i<5; i++){
+			days.add(new Day());
+		}
+	}
+	
+	public Day getDay(int day){
+		return days.get(day);
+	}
+
 }
